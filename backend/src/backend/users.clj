396e12b65e8dce1fc30df53831be52e053dd9ad9
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-03-14 15:59:41 +0000

    write to a real database

--- backend/src/backend/users.clj
+++ backend/src/backend/users.clj
@@ -1,5 +1,8 @@
 (ns backend.users
-  (:require [backend.db :refer :all]))
+  (:require [backend.db :as bdb]
+            [clojure.java.jdbc :as j]))
+
+(def USE-DB true)
 
 (def users (atom {}))
 
@@ -9,12 +12,20 @@
 (reset-users!)
 
 (defn user-exist [user]
-  (@users user))
+  (if USE-DB
+    (let [query (format "select * from users where username='%s'" user)
+          result (j/query bdb/db [query] :row-fn :cost)]
+      (> (count result) 0))
+    (@users user)))
 
+;TODO: what if we make a new database every single time??
 (defn create-user [user password]
-  (swap! users
-         (fn [m] (assoc m user {:password password}))))
-
+  (if USE-DB
+    (j/insert! bdb/db "users" {:username user :password password})
+    (swap! users
+           (fn [m] (assoc m user {:password password})))))
 
 (defn check-password [user password]
   (= (:password (@users user)) password))
+
+;TODO: should I define a protocol for a mock that behaves a bit like a databse?
