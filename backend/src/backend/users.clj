fbf4fdd810e00447c2527baf09bc0507277af239
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-03-14 19:57:10 +0000

    merge db file into users

--- backend/src/backend/users.clj
+++ backend/src/backend/users.clj
@@ -1,24 +1,42 @@
 (ns backend.users
-  (:require [backend.db :as bdb]
-            [clojure.java.jdbc :as j]))
+  (:require [clojure.java.jdbc :as j]))
 
-(def users (atom {}))
+(def db
+  {:classname   "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname     "db/database.db"
+   })
 
 (defn reset-users! []
-  (bdb/reset-users!))
+  []
+  (j/db-do-commands db ()))
 
 (reset-users!)
 
+(defn create-users-schema
+  []
+  (j/create-table-ddl :users
+                    [:password :text]
+                    [:username :text]))
+
 (defn user-exist [user]
   (let [query (format "select * from users where username='%s'" user)
-        result (j/query bdb/db [query] :row-fn :cost)]
+        result (j/query db [query] :row-fn :cost)]
     (> (count result) 0)))
 
 ;TODO: what if we make a new database every single time??
 (defn create-user [user password]
-  (j/insert! bdb/db "users" {:username user :password password}))
+  (j/insert! db "users" {:username user :password password}))
+
+
+(defn get-password [username]
+  (let
+      [querystring (format "select password from users where username='%s'" username)]
+    (:password
+     (first (j/query db
+                     querystring)))))
 
 (defn check-password [user password]
-  (= (bdb/get-password user) password))
+  (= (get-password user) password))
 
 ;TODO: should I define a protocol for a mock that behaves a bit like a databse?
