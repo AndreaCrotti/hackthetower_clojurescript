c5235d9c8a5b786a0a5c97460ee6cb7d050b4c3e
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-03-14 21:09:06 +0000

    guess words logic

--- backend/src/backend/hangman.clj
+++ backend/src/backend/hangman.clj
@@ -1,10 +1,30 @@
 (ns backend.hangman
   (:require [clojure.string :as str]))
 
+(def secret-word (atom ""))
+(def dictionary-file "/usr/share/dict/british")
+(def all-words (str/split-lines (slurp dictionary-file)))
 ;; Generate a random string which has to be guessed by different users
+
+(defn guess-word
+  [word]
+  (= word @secret-word))
+
+(defn pick-random-element
+  [coll]
+  (let [size (count coll)
+        index (Math/round (* (Math/random) (dec size)))]
+    (nth coll index)))
 
 (defn gen-string
   "Generate a random string"
   [dictionary n]
-  (let [index (- (* n (Math/random)) 1)]
-    (nth (filter #(= (count %) n) dictionary) index)))
+  (pick-random-element (filter #(= (count %) n) dictionary)))
+
+(defn secret-string
+  [secret]
+  (str/join (map #(if (:visible %) (:char %) \_) secret)))
+
+(defn reveal-letter
+  [secret letter]
+  (map #(if (= (:char %) letter) {:char letter :visible true} %) secret))
--- backend/test/backend/hangman_test.clj
+++ backend/test/backend/hangman_test.clj
@@ -9,7 +9,28 @@ sample
 something
 ")
 
+
+(def sample-secret
+  [{:char \x :visible false}
+   {:char \y :visible true}
+   {:char \z :visible false}])
+
+
 (deftest hangman-test
+  (testing "Guess string"
+    (reset! secret-word "abc")
+    (true? (guess-word "abc")))
+
+  (testing "Random element"
+    ;; (doseq [i (range 10)]
+    (pick-random-element ["abc" "bde"]))
+
+  (testing "Mask"
+    (is (= (secret-string sample-secret) "_y_")))
+
+  (testing "reveal"
+    (is (= (secret-string (reveal-letter sample-secret \x)) "xy_")))
+
   (testing "Generation"
     (let [sample (gen-string (str/split-lines sample-dictionary) 3)]
       (is (= "abc" sample)))))
