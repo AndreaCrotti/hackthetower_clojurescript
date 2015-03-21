a92805ee1ac4da661f06a8c11e1645de2b62de6b
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-03-21 12:45:00 +0000

    fix naming

--- backend/src/backend/game.clj
+++ backend/src/backend/game.clj
@@ -16,22 +16,24 @@
   (nth (read-line) 0))
 
 
-(defn loop
-  [& {:keys [attempt] :or {attempt 0}}]
+(defn game-loop
+  [limit & {:keys [attempt] :or {attempt 0}}]
   (if (game-over @masked-word)
     (println (format "Game finished, answer was %s" @secret-word))
-    (do
-      ;; (move (random-char))
-      (move (get-letter))
-      (println (format "At attempt %d now word is %s" attempt (secret-string @masked-word)))
-      (if (attempt-guess)
-        (println "Congratulations you got the right word")
-        (loop :attempt (inc attempt))))))
+    (if (= limit attempt)
+      (println "Sorry no more attempts av")
+
+      (do
+        (move (get-letter))
+        (println (format "At attempt %d now word is %s" attempt (secret-string @masked-word)))
+        (if (attempt-guess)
+          (println "Congratulations you got the right word")
+          (game-loop limit :attempt (inc attempt)))))))
 
 ;TODO: for the parallel computation create one string for every game
 (defn -main
   "Application main function"
   [& args]
   (set-secret 10)
-  (loop :attempt 0))
+  (game-loop 10 :attempt 0))
 
