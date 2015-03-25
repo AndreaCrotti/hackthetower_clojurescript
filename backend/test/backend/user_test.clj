ea26f45f64a373e661a0a2887d280f7767e1f700
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-03-25 10:07:10 +0000

    delete unused file

--- backend/test/backend/user_test.clj
+++
@@ -1,8 +0,0 @@
-(ns backend.user-test
-  (:require [clojure.test :refer :all]
-            [backend.users :refer :all]))
-
-(deftest users-test
-  (testing "check-pass"
-    (create-user "user" "pass")
-    (is (= true (check-password "user" "pass")))))
