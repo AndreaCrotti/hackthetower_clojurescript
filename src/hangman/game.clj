6d0dd15d26e2a387980c4f056382897bd5d3b754
liamjtaylor <liam.taylor@uswitch.com>
2015-04-25 21:57:37 +0100

    added new features
    Revealed answer after loss, revealed word letter count when starting game, error reported back when no letter entered by user, length of word randomised between 6 and 15 from new updated library

--- src/hangman/game.clj
+++ src/hangman/game.clj
@@ -9,6 +9,7 @@
 (def game-id (atom nil))
 
 
+
 ;TODO: add logging and other things
 (defn get-letter
   "Get a letter that is not already used"
@@ -16,16 +17,19 @@
   (let [valid-chars (available-letters @game-id)]
     (println "What letter you want to reveal? Possible choices = " (clojure.string/join " " valid-chars))
     (let [user-input (read-line)]
-      (if (> (count user-input) 1)
-        (do
-          (println "Input too log")
-          (get-letter))
-        (let [char (nth user-input 0)]
-          (if (contains? valid-chars char)
-            char
-            (do
-              (println "Not a valid choice")
-              (get-letter))))))))
+      ;both entries of zero and above 1 now give errors
+      (if (or (> (count user-input) 1) (< (count user-input) 1))
+         (do
+           (println "Invalid input")
+           (get-letter))
+             (let [char (nth user-input 0)]
+                (if (contains? valid-chars char)
+                 char
+                   (do
+                     (println "Not a valid choice")
+                     (get-letter))))))))
+
+
 
 
 (defn game-loop
@@ -34,44 +38,9 @@
   (if (game-over @game-id)
     (println "Congratulations, You won!")
     (if (= limit (attempts @game-id))
-      (println "Game over sorry")
+      ;revealed answer after game loss
+      (println "Game over sorry, the word was" (initial-secret @game-id))
       (do
         (let [new-secret (reveal-letter @game-id (get-letter))]
           (println "at attempt " (attempts @game-id) " the secret is " new-secret)
           (game-loop limit))))))
-
-(def cli-options
-  ;; An option with a required argument
-  [["-n" "--attempts ATTEMPT" "Number of max attempts"
-    :default 10
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(pos? %) "Must be a positive number"]]
-
-   ["-l" "--length LENGTH" "Random string length"
-    :default 10
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(pos? %) "Must be a positive number"]]
-
-   ["-s" "--secret" "Optional string to set"]
-
-   ["-v" nil "Verbosity level"
-    :id :verbosity
-    :default 0
-    :assoc-fn (fn [m k _] (update-in m [k] inc))]
-   ;; A boolean option defaulting to nil
-   ["-h" "--help"]])
-
-
-(defn -main
-  "Application main function"
-  [& args]
-  (let [options (parse-opts args cli-options)
-        length (-> options :options :length)
-        attempts (-> options :options :attempts)
-        current-game-id (new-game :secret (wordgen/gen-string wordgen/all-words length))]
-    
-    (if (-> options :options :help)
-      (clojure.pprint/pprint cli-options)
-      (do
-        (reset! game-id current-game-id)
-        (game-loop attempts)))))
