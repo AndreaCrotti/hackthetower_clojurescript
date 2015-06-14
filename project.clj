1c96c838ea920d63189e15042359cf5057c1fd76
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-06-14 13:30:26 +0100

    add more libraries and reorder them

--- project.clj
+++ project.clj
@@ -5,32 +5,43 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"] ;TODO: try clojure 1.7 and add core.async
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.memoize "0.5.7"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/data.json "0.2.6"]
+
                  [com.cemerick/friend "0.2.1"]
-                 [quil "2.2.6"]
-                 [expectations "2.1.1"]
                  [clj-jwt "0.0.13"]
+
+                 [play-clj "0.4.6"]
+                 [quil "2.2.6"]
+
+                 [expectations "2.1.1"]
+
                  [http-kit "2.1.19"]
+
                  [hiccup "1.0.5"]
                  [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]
+
+                 [org.xerial/sqlite-jdbc "3.8.10.1"]
                  [org.clojure/java.jdbc "0.3.7"]
+
                  [metosin/ring-swagger "0.20.4"]
                  [metosin/ring-swagger-ui "2.1.8-M1"]
                  [metosin/ring-http-response "0.6.2"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring-middleware-format "0.5.0" :exclusions [ring]]
+
                  [jarohen/phoenix "0.1.2"]
                  [com.stuartsierra/component "0.2.3"]
-                 [play-clj "0.4.6"]
+
                  [compojure "1.3.4"]
                  [bidi "1.19.0"]
+
                  [environ "1.0.0"]
-                 [org.clojure/core.memoize "0.5.7"]
-                 [slamhound "1.5.5"]
-                 [ring-middleware-format "0.5.0" :exclusions [ring]]
-                 [org.clojure/tools.cli "0.3.1"]
-                 [org.clojure/data.json "0.2.6"]
-                 [ring/ring-defaults "0.1.5"]
-                 ;TODO: add postgres and datomic
-                 [org.xerial/sqlite-jdbc "3.8.10.1"]]
+                 [slamhound "1.5.5"]]
   
   :plugins [[lein-ring "0.8.13"]
             [lein-expectations "0.0.8"]
