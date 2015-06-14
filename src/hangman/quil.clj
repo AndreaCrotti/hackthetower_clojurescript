bc6682fe65783213d5ccb86c67b9b63f625a26ff
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-06-14 14:06:48 +0100

    more quil

--- src/hangman/quil.clj
+++ src/hangman/quil.clj
@@ -1,6 +1,14 @@
 (ns hangman.quil
   (:require [quil.core :as q]))
 
+(def WIDTH 640)
+(def HEIGHT 480)
+
+(def showing-order
+  {0 {:base (fn [] (q/box 10))}
+   1 {:stick (fn [] (q/line HEIGHT (/ WIDTH 2)))}
+   2 {:hook (fn [] (q/line 1 2))}})
+
 
 (defn setup []
   (q/smooth)
@@ -8,7 +16,12 @@
   (q/background 200))
 
 
-(defn draw []
+(def base []
+  )
+
+(defn draw
+  [steps]
+  (q/ellipse 100 100 100 100)
   (q/line 0 0 10 10))
 
 
---
+++ test/hangman/quil_test.clj
@@ -0,0 +1,9 @@
+(ns hangman.quil-test
+  (:require [hangman.quil :refer :all]
+            [clojure.test :refer :all]))
+
+
+(deftest quil-test
+  (testing "1 to many steps")
+  (testing "box coordinates"
+    ()))
