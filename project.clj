514359f2741f0363798e2ba9101a52afd386d984
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-08-09 00:06:36 +0100

    update a few libraries

--- project.clj
+++ project.clj
@@ -9,15 +9,14 @@
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/core.memoize "0.5.7"]
-                 [org.clojure/tools.cli "0.3.1"]
+                 [org.clojure/tools.cli "0.3.2"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/core.typed "0.3.0"]
+                 [org.clojure/core.typed "0.3.10"]
 
                  [prismatic/schema "0.4.3"]
                  [com.cemerick/friend "0.2.1"]
                  [clj-jwt "0.1.0"]
 
-                 [play-clj "0.4.6"]
                  [quil "2.2.6"]
                  [inkwell "0.1.1"]
 
@@ -29,20 +28,19 @@
                  [hiccup "1.0.5"]
                  [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]
 
-                 [org.xerial/sqlite-jdbc "3.8.10.1"]
-                 [org.clojure/java.jdbc "0.3.7"]
+                 [org.xerial/sqlite-jdbc "3.8.11.1"]
+                 [org.clojure/java.jdbc "0.4.1"]
 
                  [metosin/ring-swagger "0.20.4"]
                  [metosin/ring-swagger-ui "2.1.8-M1"]
-                 [metosin/ring-http-response "0.6.2"]
+                 [metosin/ring-http-response "0.6.3"]
                  [ring/ring-defaults "0.1.5"]
                  [ring-middleware-format "0.5.0" :exclusions [ring]]
 
                  [jarohen/phoenix "0.1.2"]
                  [com.stuartsierra/component "0.2.3"]
 
-                 [compojure "1.3.4"]
-                 [bidi "1.20.0"]
+                 [compojure "1.4.0"]
 
                  [environ "1.0.0"]
                  [slamhound "1.5.5"]]
@@ -57,5 +55,4 @@
   ;; :aliases {"test" ["expectations"]}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [midje "1.6.3"]
                         [ring-mock "0.1.5"]]}})
