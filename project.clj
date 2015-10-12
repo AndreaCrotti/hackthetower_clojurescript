1e336c31ee51758ee5c3c01af6d4fe28ddbe0b92
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-10-12 10:24:58 +0100

    req

--- project.clj
+++ project.clj
@@ -13,38 +13,38 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.typed "0.3.11"]
 
-                 [prismatic/schema "0.4.4"]
+                 [prismatic/schema "1.0.1"]
                  [com.cemerick/friend "0.2.1"]
-                 [clj-jwt "0.1.0"]
+                 [clj-jwt "0.1.1"]
 
                  [quil "2.2.6"]
                  [inkwell "0.1.1"]
 
                  [expectations "2.1.3"]
-                 [org.clojure/test.check "0.8.1"]
+                 [org.clojure/test.check "0.8.2"]
 
                  [http-kit "2.1.19"]
 
                  [hiccup "1.0.5"]
                  [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]
 
-                 [org.xerial/sqlite-jdbc "3.8.11.1"]
+                 [org.xerial/sqlite-jdbc "3.8.11.2"]
                  [org.postgresql/postgresql "9.2-1003-jdbc4"]
-                 [org.clojure/java.jdbc "0.4.1"]
-                 [yesql "0.5.0"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [yesql "0.5.1"]
 
-                 [metosin/ring-swagger "0.20.4"]
+                 [metosin/ring-swagger "0.21.0"]
                  [metosin/ring-swagger-ui "2.1.8-M1"]
-                 [metosin/ring-http-response "0.6.4"]
+                 [metosin/ring-http-response "0.6.5"]
                  [ring/ring-defaults "0.1.5"]
-                 [ring-middleware-format "0.5.0" :exclusions [ring]]
+                 [ring-middleware-format "0.6.0" :exclusions [ring]]
 
                  [jarohen/phoenix "0.1.2"]
-                 [com.stuartsierra/component "0.2.3"]
+                 [com.stuartsierra/component "0.3.0"]
 
                  [compojure "1.4.0"]
 
-                 [environ "1.0.0"]
+                 [environ "1.0.1"]
                  [slamhound "1.5.5"]]
   
   :phoenix/config "hangman.edn"
