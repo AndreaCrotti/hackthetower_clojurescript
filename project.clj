916af302540afaf4f30909dfbbb0407ec45b8690
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-06-16 14:43:26 +0100

    new deps

--- project.clj
+++ project.clj
@@ -7,17 +7,17 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/core.async "0.2.391"]
+                 [org.clojure/core.async "0.3.443"]
                  [org.clojure/core.memoize "0.5.9"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/core.typed "0.3.28"]
+                 [org.clojure/core.typed "0.3.32"]
 
-                 [prismatic/schema "1.1.3"]
+                 [prismatic/schema "1.1.6"]
                  [com.cemerick/friend "0.2.3"]
                  [clj-jwt "0.1.1"]
 
-                 [quil "2.4.0"]
+                 [quil "2.6.0"]
                  [inkwell "0.1.1"]
 
                  [expectations "2.1.9"]
@@ -28,21 +28,21 @@
                  [hiccup "1.0.5"]
                  [clj-tagsoup "0.3.0" :exclusions [org.clojure/clojure]]
 
-                 [org.xerial/sqlite-jdbc "3.8.11.2"]
-                 [org.postgresql/postgresql "9.4.1210"]
+                 [org.xerial/sqlite-jdbc "3.18.0"]
+                 [org.postgresql/postgresql "42.1.1"]
                  [org.clojure/java.jdbc "0.6.1"]
                  [yesql "0.5.3"]
 
-                 [metosin/ring-swagger "0.22.10"]
-                 [metosin/ring-swagger-ui "2.2.2-0"]
-                 [metosin/ring-http-response "0.8.0"]
-                 [ring/ring-defaults "0.2.1"]
-                 [ring-middleware-format "0.7.0" :exclusions [ring]]
+                 [metosin/ring-swagger "0.24.0"]
+                 [metosin/ring-swagger-ui "2.2.10"]
+                 [metosin/ring-http-response "0.9.0"]
+                 [ring/ring-defaults "0.3.0"]
+                 [ring-middleware-format "0.7.2" :exclusions [ring]]
 
                  [jarohen/phoenix "0.1.3"]
-                 [com.stuartsierra/component "0.3.1"]
+                 [com.stuartsierra/component "0.3.2"]
 
-                 [compojure "1.5.1"]
+                 [compojure "1.6.0"]
 
                  [environ "1.1.0"]
                  [slamhound "1.5.5"]]
